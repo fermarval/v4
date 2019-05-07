59c17f4933e36a014c63c3f99080d3f039552b2e
Fernando Marmolejo <fmarvallejo@gmail.com>
2019-05-08 01:27:01 +0200

    no message

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,24 +1,7 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g id="Logo" transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-       <text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_1" y="129.4375" x="61.5" stroke-width="0" stroke="#000" fill="#64FFDA">FM</text>
-          
-        
-        <polygon
-          id="Shape"
-          stroke="#64FFDA"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-      </g>
-    </g>
-  </svg>
+  <p>TEST</p>
 );
 
 export default IconLogo;
